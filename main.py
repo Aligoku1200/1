fcb4c71ebc3d031993aaef06250b6a2e7ab0b23b
Aligoku1200 <106703834+Aligoku1200@users.noreply.github.com>
2022-06-09 16:57:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 from os import system as Z
-#from keep_alive import keep_alive
-Z("/opt/virtualenvs/python3/bin/python3 -m pip install --upgrade pip")
-#keep_alive()
-Z("clear")
 Z("python coins.py")
